27df30c7ccbf4cf0665b3a5c32370a665908f469
@ppwicho <luis89@gmail.com>
2021-06-08 19:08:32 -0500

    angular material

---
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Params} from '@angular/router';
+
+import {ProductsService} from '../../../core/service/products/products.service';
+
+import {Product} from '../../../product.model'
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  product : Product;
+
+  constructor(
+    private route:ActivatedRoute,
+    private productService:ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params:Params) => {
+      
+      const id = params.id;
+      console.log(id);
+      this.product=this.productService.getProduct(id);
+
+    });
+  }
+
+}
