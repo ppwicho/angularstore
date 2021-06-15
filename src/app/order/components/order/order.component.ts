add7ebff74d78a69622632d1b4e5947b4c2e96d1
@ppwicho <luis89@gmail.com>
2021-06-14 20:36:45 -0500

    order page

---
+++ src/app/order/components/order/order.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/core/services/cart.service';
+import { Product } from 'src/app/product.model';
+import { Observable } from 'rxjs'
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss']
+})
+export class OrderComponent implements OnInit {
+
+  products$: Observable<Product[]>;
+
+  constructor(
+    private cartService : CartService
+  ) {
+    this.products$=this.cartService.cart$;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
